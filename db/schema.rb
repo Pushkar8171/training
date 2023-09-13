11ef561be891fce5ea8a75196b34993353653d1f
Pushkar8171 <pushkarsinghrathore81@gmail.com>
2023-09-13 16:21:57 +0530

    Newly implemented Hasmany relationship on the model Books & Authors- where a author can have multiple Books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_085844) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_13_104514) do
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
