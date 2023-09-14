56f89a369033fccebaf12131b1d3268c61f30a3c
Pushkar8171 <pushkarsinghrathore81@gmail.com>
2023-09-14 13:20:49 +0530

    Undersatnding Association :hasMany in better way

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_114615) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_14_062503) do
   create_table "accounts", force: :cascade do |t|
     t.integer "account_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "supplier_id"
     t.index ["supplier_id"], name: "index_accounts_on_supplier_id"
+  end
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "appointment_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "physician_id"
+    t.integer "patient_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+    t.index ["physician_id"], name: "index_appointments_on_physician_id"
   end
 
   create_table "articles", force: :cascade do |t|
@@ -55,6 +65,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_114615) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "physicians", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "suppliers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -70,6 +92,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_114615) do
   end
 
   add_foreign_key "accounts", "suppliers"
+  add_foreign_key "appointments", "patients"
+  add_foreign_key "appointments", "physicians"
   add_foreign_key "articles", "users"
   add_foreign_key "books", "authors"
 end
